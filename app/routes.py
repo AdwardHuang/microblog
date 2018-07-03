98e04afaec32d2cf81a78ae9d096b70b88774572
h <h@gmail.com>
2018-07-03 09:59:13 +0800

    chapter6 before Profile Editor

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from app import app
+from app import app, db
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import request
 from werkzeug.urls import url_parse
 from app.forms import LoginForm
 from app.models import User
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
